b21badc6c0a8bf04382a411fbc36ed504e5299db
Ethan Chien <liangtie.qian@gmail.com>
2023-12-24 16:20:38 +0800

    Revert "feat: Add config for zones_manager"
    This reverts commit 5327afb575425f6f2023f20f2f3e4525c31f11c9.

--- build.ps1
+++ build.ps1
@@ -100,7 +100,7 @@ param(
     [Parameter(Mandatory = $False, ParameterSetName = "package")]
     [Parameter(Mandatory = $False, ParameterSetName = "preparepackage")]
     # NOTE Change to your build config
-    [string]$BuildConfigName = 'kicad-zones_manager',
+    [string]$BuildConfigName = 'kicad-dev',
 
     [Parameter(Mandatory = $False, ParameterSetName = "build")]
     [Parameter(Mandatory = $False, ParameterSetName = "package")]
